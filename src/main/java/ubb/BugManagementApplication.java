a79ff37f276d88149ed437f3c8f46e03be40de40
StefanButacu <stefbutacu@gmail.com>
2022-04-27 17:01:48 +0300

    Initial commit

---
+++ src/main/java/ubb/BugManagementApplication.java
@@ -0,0 +1,13 @@
+package ubb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ubb/BugManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ubb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
